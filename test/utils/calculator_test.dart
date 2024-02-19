5fe5814ff7db4dc97804fa8054c767d91714cb2e
Prathamesh Rokade <prathameshr955@gmail.com>
2024-02-19 20:27:01 +0530

    Dependency Mocking using Mocktail

--- test/utils/calculator_test.dart
+++ test/utils/calculator_test.dart
@@ -1,5 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ibmi/utils/calculator.dart';
+import 'package:mocktail/mocktail.dart';
+
+class DioMock extends Mock implements Dio {}
 
 void main() {
   test(
@@ -11,5 +15,26 @@ void main() {
     double bmi = calculateBMI(height, weight);
     //Assert
     expect(bmi, 22.955102040816328);
+  });
+
+  test('Given url When calculateBMIAsync invoked Then correct BMI returned',
+      () async {
+    //Arrange
+    final _dioMock = DioMock();
+    when(() => _dioMock.get('https://www.jsonkeeper.com/b/AKFA')).thenAnswer(
+      (_) => Future.value(
+        Response(
+          requestOptions:
+              RequestOptions(path: 'https://www.jsonkeeper.com/b/AKFA'),
+          data: {
+            "Sergio Ramos": [72, 165]
+          },
+        ),
+      ),
+    );
+    //Act
+    var _result = await calculateBMIAsync(_dioMock);
+    //Assert
+    expect(_result, 22.375578703703706);
   });
 }
